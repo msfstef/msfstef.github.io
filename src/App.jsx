4909eeed6439c12b04716903bd4c5539fa5d5897
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-01-26 16:26:43 +0200

    Initial commit for React App

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+              Learn React
+          </header>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
