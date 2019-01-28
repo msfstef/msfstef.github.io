a291c052ebb8aa500a3b9b4ca48675e003a4fdaf
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-01-28 11:54:53 +0200

    moving to laptop work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,13 +7,6 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-              Learn React
-          </header>
         </div>
       </Router>
     );
