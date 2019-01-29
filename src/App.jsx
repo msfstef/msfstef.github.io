e8fb053da326be135f58cdcee9eb17e2666bff93
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-01-29 16:23:05 +0200

    working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Landing from './components/Landing';
 import Work from './components/Work';
+import Header from './components/Header'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <Header />
           <Route exact path='/' component={Landing} />
           <Route exact path='/work' component={Work} />
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 class Header extends Component {
+    state = {
+        toggle: false
+    }
+    
+    toggleMenu = () => {
+        this.setState({toggle: !this.state.toggle})
+    }
+
     render() {
+        if (window.location.pathname === '/') {
+            return <div></div>
+        }
+
         return (
+            
             <div className='Header'>
+                <img className="navbar logo"
+                    src={'/assets/logo.svg'} alt="msfstef logo"/>
                 
+                <div className={"navbar menuicon " + (this.state.toggle?"change":"")}
+                    onClick={()=>this.toggleMenu()}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
             </div>
         );
     }
