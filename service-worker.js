711e5c61d42d008226a2f10644e32bb4e2f06100
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-01-31 00:16:08 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ec52fe820853b183114d9df631265ec.js"
+  "/precache-manifest.7d8a6a4c64340f3c0e20c3da2adae39e.js"
 );
 
 workbox.clientsClaim();
