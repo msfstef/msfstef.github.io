fdd4a80e0448eff8121193002debefcf15c07c3f
Stefanos Mousafeiris <msfstef@gmail.com>
2019-01-30 15:57:29 +0200

    Updates

---
+++ precache-manifest.ecf906f79837488006f43ace7d075b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2fea76f2993090c5f09",
+    "url": "/static/js/main.f2fea76f.chunk.js"
+  },
+  {
+    "revision": "599a0de5187fb4c690a2",
+    "url": "/static/js/1.599a0de5.chunk.js"
+  },
+  {
+    "revision": "f2fea76f2993090c5f09",
+    "url": "/static/css/main.da8a221a.chunk.css"
+  },
+  {
+    "revision": "86b43445efb0ac3883fb24499cbbaf18",
+    "url": "/index.html"
+  }
+];
