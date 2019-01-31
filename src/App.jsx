6dec13d59c6396e0b10f8d254e5cf8bf720f70e3
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-01-31 23:27:53 +0200

    sped up exit animations

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
                   classNames="page"
                   timeout={{
                     enter: 1000,
-                    exit: 1000,
+                    exit: 500,
                   }}
                 >
                   <Route
