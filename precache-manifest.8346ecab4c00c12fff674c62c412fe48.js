df6416514d634798817ca9715c97dfe31b29978c
Stefanos Mousafeiris <msfstef@gmail.com>
2019-02-01 18:12:59 +0200

    Updates

--- precache-manifest.8346ecab4c00c12fff674c62c412fe48.js
+++ precache-manifest.8346ecab4c00c12fff674c62c412fe48.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "dad3f331a71c7db9fbc6",
-    "url": "/static/js/main.dad3f331.chunk.js"
+    "revision": "ff8a456195013f51b1a7",
+    "url": "/static/js/main.ff8a4561.chunk.js"
   },
   {
     "revision": "30f220ae26e7cc4cdb18",
     "url": "/static/js/1.30f220ae.chunk.js"
   },
   {
-    "revision": "dad3f331a71c7db9fbc6",
-    "url": "/static/css/main.fc0bd504.chunk.css"
+    "revision": "ff8a456195013f51b1a7",
+    "url": "/static/css/main.ba97cbf4.chunk.css"
   },
   {
-    "revision": "1d28be7f2ed70c53c9c05a7670fc50f9",
+    "revision": "a404316f8612ea5b8f0c62b39a6ba52d",
     "url": "/index.html"
   }
 ];
