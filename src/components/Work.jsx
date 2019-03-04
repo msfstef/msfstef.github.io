688b74d21e393eac1cd3119a4888bd397acbddf0
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-03-04 12:14:40 +0200

    changed desktop portfolio layout

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,37 +3,73 @@ import WorkItem from './WorkItem'
 import './Work.css'
 
 class Work extends Component {
+    componentDidMount() {
+        (function() {
+            function scrollHorizontally(e) {
+                e = window.event || e;
+                var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+                document.getElementById('WorkWrapper').scrollLeft -= (delta*70);
+                e.preventDefault();
+            }
+            if (document.getElementById('WorkWrapper').addEventListener) {
+                // IE9, Chrome, Safari, Opera
+                document.getElementById('WorkWrapper').addEventListener("mousewheel", scrollHorizontally, false);
+                // Firefox
+                document.getElementById('WorkWrapper').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+            } else {
+                // IE 6/7/8
+                document.getElementById('WorkWrapper').attachEvent("onmousewheel", scrollHorizontally);
+            }
+        })();
+
+        const slider = document.getElementById('WorkWrapper');
+        var isDown = false;
+        var startX;
+        var scrollLeft;
+
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            slider.classList.add('active');
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mousemove', (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) * 2; //scroll-fast
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    }
+
     render() {
         return (
-            <div className='Work wrapper'>
+            <div id="WorkWrapper" className='Work wrapper'>
                 <WorkItem
-                        itemName='crosspix' 
-                        itemTitle='CrossPiX'
-                        itemDesc='Web app to convert pictures to cross-stitch patterns.'
-                        link='https://msfstef.github.io/CrossPiX' />
+                itemName='crosspix' 
+                itemTitle='CrossPiX'
+                itemDesc='Web app to convert pictures to cross-stitch patterns.'
+                link='https://msfstef.github.io/CrossPiX' />
                 <WorkItem
-                        itemName='headbattle'
-                        itemTitle='Headbattle'
-                        itemDesc='A plain JS 1v1 football inspired game.'
-                        link="https://msfstef.github.io/headbattle"/>
-                <WorkItem
-                        itemName='headbattle'
-                        itemTitle='Headbattle'
-                        itemDesc='A plain JS 1v1 football inspired game.'
-                        link="https://msfstef.github.io/headbattle"/>
-                <WorkItem
-                        itemName='headbattle'
-                        itemTitle='Headbattle'
-                        itemDesc='A plain JS 1v1 football inspired game.'
-                        link="https://msfstef.github.io/headbattle"/>
-                <WorkItem
-                        itemName='headbattle'
-                        itemTitle='Headbattle'
-                        itemDesc='A plain JS 1v1 football inspired game.'
-                        link="https://msfstef.github.io/headbattle"/>
+                itemName='headbattle'
+                itemTitle='Headbattle'
+                itemDesc='A plain JS 1v1 football inspired game.'
+                link="https://msfstef.github.io/headbattle"/>
+
             </div>
         );
     }
 }
 
 export default Work;
+
+
+
